3c3f6de7a62ba8000575bf5d5148c0f17250a19a
wahyu97 <wahyuuntoro54@gmail.com>
2017-03-19 19:38:13 +0800

    Add files via upload

---
+++ myphow/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<?php
+echo "<h2>Form Pendaftaran</h2>";
+echo $_POST['nama'];
+echo "<br>";
+echo $_POST['paket'];
+echo "<br>";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['notelp'];
+echo "<br>";
+echo $_POST['jumlah'];
+echo "<br>";
+echo $_POST['tglbrngkt'];
+?>
+<body>
+</body>
+</html>
